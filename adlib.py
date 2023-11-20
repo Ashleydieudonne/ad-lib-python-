fefb4e21100409ee645771d807787d96be8e2b6f
Ashley Dieudonne <128640662+Ashleydieudonne@users.noreply.github.com>
2023-11-19 20:28:59 -0600

    Add files via upload

---
+++ adlib.py
@@ -0,0 +1,10 @@
+name = input("Name: ")
+age = input("Age: ")
+adj = input("Adjective that describes you: ")
+hobby1 = input("1st Hobby: ")
+hobby2 = input("2nd Hobby: ")
+hobby3 = input("3rd Hobby: ")
+
+adlib = f"Hello my name is {name} and I am {age} years old I like to {hobby1}, {hobby2}, and {hobby3}.\
+if I had to choose one word to describe myself I would say I am {adj} "
+print(adlib)
